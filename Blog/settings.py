12d6e04d11628f42c3c2de5b4204d7cc4d630dfe
Gaurav Kumar Tailor <gauravtailorthoi@gmail.com>
2021-07-30 10:09:54 +0530

    deploy it

--- Blog/settings.py
+++ Blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^2r*x(m$oywr$_&&)sbsq(rt6guk+qz1#1w0panqx1gdl)czf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogpostwebapplication.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
